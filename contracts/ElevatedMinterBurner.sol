82d656040893d29ab62b5497a4f90b55803be18c
grobelr <grobelr@gmail.com>
2025-07-04 13:11:01 -0300

    fix: adjust the IMintableBurnable interface and remove the return from the functions

--- contracts/ElevatedMinterBurner.sol
+++ contracts/ElevatedMinterBurner.sol
@@ -2,7 +2,24 @@
 pragma solidity ^0.8.0;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { IMintableBurnable } from "@layerzerolabs/oft-evm/contracts/interfaces/IMintableBurnable.sol";
+// import { IMintableBurnable } from "@layerzerolabs/oft-evm/contracts/interfaces/IMintableBurnable.sol";
+
+interface IMintableBurnable {
+    
+    /**
+     * @notice Burns tokens from a specified account
+     * @param _from Address from which tokens will be burned
+     * @param _amount Amount of tokens to be burned
+     */
+    function burn(address _from, uint256 _amount) external;
+
+    /**
+     * @notice Mints tokens to a specified account
+     * @param _to Address to which tokens will be minted
+     * @param _amount Amount of tokens to be minted
+     */
+    function mint(address _to, uint256 _amount) external;
+}
 
 contract ElevatedMinterBurner is IMintableBurnable, Ownable {
     IMintableBurnable public immutable token;
@@ -21,11 +38,11 @@ contract ElevatedMinterBurner is IMintableBurnable, Ownable {
         operators[_operator] = _status;
     }
 
-    function burn(address _from, uint256 _amount) external override onlyOperators returns (bool) {
-        return token.burn(_from, _amount);
+    function burn(address _from, uint256 _amount) external override onlyOperators {
+        token.burn(_from, _amount);
     }
 
-    function mint(address _to, uint256 _amount) external override onlyOperators returns (bool) {
-        return token.mint(_to, _amount);
+    function mint(address _to, uint256 _amount) external override onlyOperators {
+        token.mint(_to, _amount);
     }
 }
